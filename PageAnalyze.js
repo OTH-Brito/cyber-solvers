aa42752f034e57d581b126cb3d0b8bfe6a03c399
lage-bonde <138974670+lage-bonde@users.noreply.github.com>
2023-07-30 18:58:40 +0200

    Is this pageanalyze algo working?

--- PageAnalyze.js
+++ PageAnalyze.js
@@ -10,7 +10,22 @@ function PageAnalyze(
     authourEmailAdresses = [];
 
     // TODO: Detecting if viewing an email or not.
-    // TODO: Filtering and stuff here
+    // TODO: Filtering and stuff here (ONLY FOR LINKS)
+
+
+    var emailMessages = WebPageData.match(/class="adn ads".*/g);                    // Finds the tags representing email messages in the conversation
+
+    for (let message = 0; message < emailMessages.length; message++) {
+        var element_H3_IW = /<h3.*class="iw".*/i.exec(emailMessages[message]);      // Finds the h3.iw tag
+        var element_SPAN = /email="[^"]*"/i.exec(element_H3_IW).toString();         // Finds the email attribute in the span tag
+
+        var formattingEmailAddress = element_SPAN.substring(                        // Formatting the email
+            element_SPAN.indexOf("\"")+1,
+            element_SPAN.lastIndexOf("\"")
+        );
+
+        authourEmailAdresses.push(formattingEmailAddress);
+    }
     
 
     return [
